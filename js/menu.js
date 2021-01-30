68aae87dbdcdfe714b3f0bf5347db8b5880b4598
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2021-01-30 17:28:10 +0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -12,11 +12,10 @@ var Menu={
 		var score1;
 		var len = instruction.length;
 		//VK.api("storage.get",{ "key": "top1","global": 1},function(data){score1=data.response;VK.api("storage.get",{ "key": "name1","global": 1},function(data){instruction.text=String(instruction.text+"\nЛидер: "+data.response+" "+score1);});});
-		VK.api("account.getProfileInfo",{},function(data){
-			console.log(JSON.stringify(data));
-			//name=String(data['response']['last_name']+" "+data['response']['first_name']);
+		VK.api("users.get",{},function(data){
+			name=String(data[0].last_name+" "+data[0].first_name);
+			game.input.onDown.add(Menu.startGame,Menu);
 		});
-		game.input.onDown.add(Menu.startGame,Menu);
 		instruction.addColor(0x4282D3,len);
 	},
 	startGame: function(){
