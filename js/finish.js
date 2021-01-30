6a0b2d734d6c322717b385ad08c52e3aad197711
Bulat102 <36506617+Bulat102@users.noreply.github.com>
2021-01-30 14:41:09 +0500

    Update finish.js

--- js/finish.js
+++ js/finish.js
@@ -19,7 +19,6 @@ var Finish ={
 		retryBut.inputEnabled=true;
 		postBut.events.onInputDown.add(this.wallPost,this);
 		retryBut.events.onInputDown.add(function(){game.state.start("Game");},this);
-		name=String(data.response.name[0].last_name+" "+data.response.name[0].first_name);
 		let code=String('return{"name":API.users.get()};');
 		VK.api("execute",{"code":code},function(data){
 			name=String(data.response.name[0].last_name+" "+data.response.name[0].first_name);
